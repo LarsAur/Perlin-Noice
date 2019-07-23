6928d22123dc27968356d3043a1af2ec81a12d61
LarsAur <lars1998@hotmail.com>
2019-07-24 01:24:26 +0200

    Generate gradient grid

---
+++ src/PerlinNoice.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class PerlinNoice {
+
+      RandomUnitVector[][] grad;
+      int sizex, sizey;
+      long seed;
+      Random rand;
+
+      public PerlinNoice(int sizex, int sizey, long seed) {
+            this.sizex = sizex;
+            this.sizey = sizey;
+            this.seed = seed;
+            rand = new Random(seed);
+
+
+            grad = new RandomUnitVector[sizex][sizey];
+
+            //TODO Use precomputed gradient vectors in hash lookup table to reduce process of creating gradient vectors
+            for (int y = 0; y < sizey; y++) {
+                  for (int x = 0; x < sizey; x++) {
+                        grad[x][y] = new RandomUnitVector(2, rand.nextLong());
+                  }
+            }
+      }
+
+      public static void main(String[] args) {
+            PerlinNoice pn = new PerlinNoice(100, 100, 0);
+      }
+
+
+}
