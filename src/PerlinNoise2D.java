47fa0c8db872bab9669229b7e16a0f48f1f2ea27
LarsAur <lars1998@hotmail.com>
2019-08-11 21:28:08 +0200

    fixed/changed chunk to vector hash method and normalized output of noise method

--- src/PerlinNoise2D.java
+++ src/PerlinNoise2D.java
@@ -5,19 +5,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
-public class PerlinNoice2D {
+public class PerlinNoise2D {
 
       private long seed;
       private Random rand;
       private int chunkSize;
 
-      public PerlinNoice2D(long seed, int chunkSize) {
+      public PerlinNoise2D(long seed, int chunkSize) {
             this.seed = seed;
             this.chunkSize = chunkSize;
             this.rand = new Random(seed);
       }
 
-      public double noice(int x, int y) {
+      public double noise(int x, int y) {
             int chunkx = Math.floorDiv(x, chunkSize);
             int chunky = Math.floorDiv(y, chunkSize);
 
@@ -26,8 +26,9 @@ public class PerlinNoice2D {
             Vector urg = chunkVectorHash(chunkx + 1, chunky);           // upper right gradient
             Vector lrg = chunkVectorHash(chunkx + 1, chunky + 1);       // lower right gradient
 
-            int inChunkx = ((x % chunkSize) + chunkSize) % chunkSize;   // mod with range [0, chunksize)
-            int inChunky = ((y % chunkSize) + chunkSize) % chunkSize;
+            int inChunkx = mod(x, chunkSize);                           // mod with range [0, chunksize)
+            int inChunky = mod(y, chunkSize);
+
             double fraqx = inChunkx / (double) chunkSize;               // fraction of x within the chunk (0=left, 1=right)
             double fraqy = inChunky / (double) chunkSize;               // fraction of y within the chunk (0=up, 1=down)
 
@@ -44,42 +45,60 @@ public class PerlinNoice2D {
             double dotur = urg.dot(urd);
             double dotlr = lrg.dot(lrd);
 
-            //Faded Lerp
-
             double lxInter = dotll + fadex * (dotlr - dotll); // lower x interpolation with fade
-            double uxInter = dotul + fadex * (dotur - dotul) ; // upper x interpolation with fade
+            double uxInter = dotul + fadex * (dotur - dotul); // upper x interpolation with fade
             double biInter = uxInter + fadey * (lxInter - uxInter); // bilinear interpolation with fade
 
-            return biInter;
+            return (biInter + 1) / 2; // Normalizing noise value to [0, 1]
       }
 
       private Vector chunkVectorHash(int cx, int cy) {
-            long chunkSeed = (long) ((Math.pow(seed - 1, cx) % 1997) * (Math.pow(seed + 1, cy) % 1999));
-            Random random = new Random(chunkSeed);
-            return predefinedVectorSet[random.nextInt(predefinedVectorSet.length)];
+
+            int k1 = cx < 0 ? -cx * 2 - 1 : cx * 2; // Mapping Integers to Natural numbers (f : Z -> N)
+            int k2 = cy < 0 ? -cy * 2 - 1 : cy * 2;
+            long chunkSeed = (((k1 + k2) * (k1 + k2 + 1)) / 2 + k2) * seed; // Cantor pairing function, seed should not be zero
+
+            // Integer hash function
+            // https://burtleburtle.net/bob/hash/integer.html
+            chunkSeed = (chunkSeed ^ 0xdeadbeef) + (chunkSeed << 4);  // deadbeef <3
+            chunkSeed = chunkSeed ^ (chunkSeed >> 10);
+            chunkSeed = chunkSeed + (chunkSeed << 7);
+            chunkSeed = chunkSeed ^ (chunkSeed >> 13);
+
+            return predefinedVectorSet[mod(chunkSeed, predefinedVectorSet.length)];
       }
 
       private static final Vector[] predefinedVectorSet = new Vector[]{
-              new Vector(new double[]{1, 0}),
-              new Vector(new double[]{0, 1}),
-              new Vector(new double[]{-1, 0}),
-              new Vector(new double[]{0, -1})
+              new Vector(new double[]{1, 1}),
+              new Vector(new double[]{-1, 1}),
+              new Vector(new double[]{-1, -1}),
+              new Vector(new double[]{1, -1}),
+              new Vector(new double[]{Math.sqrt(2), 0}),
+              new Vector(new double[]{-Math.sqrt(2), 0}),
+              new Vector(new double[]{0, -Math.sqrt(2)}),
+              new Vector(new double[]{0, Math.sqrt(2)})
       };
 
       private double fade(double t) {
             return 6 * Math.pow(t, 5) - 15 * Math.pow(t, 4) + 10 * Math.pow(t, 3);
       }
 
-      public static void main(String[] args) {
-            Random random = new Random();
-            PerlinNoice2D pn = new PerlinNoice2D(random.nextLong(), 16);
+      private int mod(long x, int m) {
+            return (int) ((x % m) + m) % m; // mod function with range [0, m) no negative numbers as with % operator
+      }
 
-            BufferedImage image = new BufferedImage(128, 128, BufferedImage.TYPE_BYTE_GRAY);
+      public static void main(String[] args) {
+            int chunkSize = 64;
+            long seed = System.nanoTime();
+            PerlinNoise2D pn = new PerlinNoise2D(seed, chunkSize);
+
+            int size = 512;
+            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
             WritableRaster raster = image.getRaster();
 
-            for (int y = 0; y < 128; y++) {
-                  for (int x = 0; x < 128; x++) {
-                        int p = (int)((pn.noice(x, y) + 1) * 127);
+            for (int y = 0; y < size; y++) {
+                  for (int x = 0; x < size; x++) {
+                        int p = (int) ((pn.noise(x, y)) * 255);
                         raster.setPixel(x, y, new int[]{p});
                   }
             }
